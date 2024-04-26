78d7a75221e21b90c69290bb25401e6bbd1ce07d
Santhanu V <vinodsanthanu@gmail.com>
2024-04-26 19:21:53 +0530

    Refactor Configuration
    Replace MachineConfig with StateConfiguration class.
    Add Transition class.
    Add interface for StateConfiguration.

---
+++ DotState/Contracts/IStateConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using dotstate;
+
+namespace DotState.Contracts;
+
+public interface IStateConfiguration<TState, TTrigger> where TState : notnull where TTrigger : notnull
+{
+    public void AddTransition(TTrigger trigger, IStateConfiguration<TState, TTrigger> destination, Func<TState, bool>? predicate = null);
+    public Transition<TState, TTrigger>? GetTransition(TTrigger trigger);
+    public TState GetState();
+}
---
+++ DotState/StateConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using DotState.Contracts;
+
+namespace dotstate;
+
+public class StateConfiguration<TState, TTrigger> : IStateConfiguration<TState, TTrigger>
+    where TState : notnull where TTrigger : notnull
+{
+    private readonly TState _state;
+    private readonly Dictionary<TTrigger, Transition<TState, TTrigger>> transitions;
+
+    public StateConfiguration(TState state)
+    {
+        transitions = new();
+        _state = state;
+    }
+
+    public void AddTransition(TTrigger trigger, IStateConfiguration<TState, TTrigger> destination, Func<TState, bool>? predicate = null)
+    {
+        Transition<TState, TTrigger> transition = predicate == null ? new(destination) : new(destination, predicate);
+        transitions.Add(trigger, transition);
+    }
+
+    public Transition<TState, TTrigger>? GetTransition(TTrigger trigger)
+    {
+        transitions.TryGetValue(trigger, out var transition);
+        return transition;
+    }
+
+    public TState GetState()
+    {
+        return _state;
+    }
+}
