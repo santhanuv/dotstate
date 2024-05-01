06b7998c9f2d25d542b1681cf0e9cf63e3111e12
Santhanu V <vinodsanthanu@gmail.com>
2024-05-01 14:01:03 +0530

    Add onEntry invokation on inital startup.

--- DotState/StateEngine.cs
+++ DotState/StateEngine.cs
@@ -8,11 +8,12 @@ public class StateEngine<TState, TTrigger> where TState : notnull where TTrigger
     private readonly StateMachine<TState, TTrigger> _machine;
     private IStateConfiguration<TState, TTrigger> _stateConfig;
 
-    public StateEngine(StateMachine<TState, TTrigger> machine, TState initialState)
+    public StateEngine(StateMachine<TState, TTrigger> machine, TState initialState, TTrigger initialTrigger)
     {
         _machine = machine;
         var stateConfig = _machine.GetStateConfiguration(initialState) ?? throw new Exception($"State \"{initialState}\" is not registered");
         _stateConfig = stateConfig;
+        _stateConfig.OnEntry?.Invoke(initialState, initialTrigger);
     }
 
     public TState ExecuteTransition(TTrigger trigger)
@@ -32,7 +33,7 @@ public class StateEngine<TState, TTrigger> where TState : notnull where TTrigger
         {
             throw new InvalidTransitionException<TState, TTrigger>(state, trigger);
         }
-        
+
         _stateConfig.OnEntry?.Invoke(state, trigger);
         return _stateConfig.GetState();
     }
