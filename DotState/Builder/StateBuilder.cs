634d5f5e375bb1620e8e654a9111e11f2c623764
Santhanu Vinod <vinodsanthanu@gmail.com>
2024-06-09 10:35:11 +0530

    Add documentation for state builder and state machine builder.

--- DotState/Builder/StateBuilder.cs
+++ DotState/Builder/StateBuilder.cs
@@ -3,20 +3,58 @@ using DotState.Exceptions;
 
 namespace DotState.Builder;
 
+/// <summary>
+/// An abstract parent builder class for states.
+/// 
+/// <see cref="CompositeStateBuilder{TState, TTrigger}"/>
+/// <see cref="ElementStateBuilder{TState, TTrigger}"/>
+/// </summary>
+/// <typeparam name="TState"></typeparam>
+/// <typeparam name="TTrigger"></typeparam>
 public abstract class StateBuilder<TState, TTrigger> : IStateBuilder<TState, TTrigger>
 {
     private readonly StateMachineBuilder<TState, TTrigger> _machineBuilder;
     private readonly IDictionary<TTrigger, TransitionBuilder<TState, TTrigger>> _transitions;
     private Action<TState, TTrigger>? _onEntry = null;
     private Action<TState, TTrigger>? _onExit = null;
-    
+
+    /// <summary>
+    /// The state associated with the builder.
+    /// </summary>
     public TState State {  get; private set; }
+
+    /// <summary>
+    /// The default state.
+    /// </summary>
+    /// <value>For element state, <c>DefaultState</c> is same as <see cref="State"/></value>
     public TState DefaultState { get; init; }
+
+    /// <summary>
+    /// The parent state.
+    /// </summary>
+    /// <value>Null if state doesn't have a parent state. By default it is set to Null.</value>
     public IStateBuilder<TState, TTrigger>? Parent { get; private set; } = null;
 
+    /// <summary>
+    /// Initializes an instance of Class <c>StateBuilder</c>
+    /// </summary>
+    /// <param name="machineBuilder">An instance of Class <c>StateMachineBuilder</c> associated with the state builder.</param>
+    /// <param name="state">The state associated with the builder.</param>
+    /// <param name="defaultState">The default state. For element state, this should be same as Param <c>state</c></param>
+    /// <param name="parent">The parent state. Default value is Null.</param>
     protected StateBuilder(StateMachineBuilder<TState, TTrigger> machineBuilder, TState state, TState defaultState, IStateBuilder<TState, TTrigger>? parent)
         :this(machineBuilder, state, defaultState, parent, null) {}
 
+    /// <summary>
+    /// Initializes an instance of Class <c>StateBuilder</c>
+    /// </summary>
+    /// <param name="machineBuilder">An instance of Class <c>StateMachineBuilder</c> associated with the state builder.</param>
+    /// <param name="state">The state associated with the builder.</param>
+    /// <param name="defaultState">The default state. For element state, this should be same as Param <c>state</c></param>
+    /// <param name="parent">The parent state.Default value is Null.</param>
+    /// <param name="transitions">
+    /// A collection containing the mapping between triggers and it's associated <c>TransitionBuilder</c>.
+    /// </param>
     protected StateBuilder(StateMachineBuilder<TState, TTrigger> machineBuilder, TState state, TState defaultState,
     IStateBuilder<TState, TTrigger>? parent, IDictionary<TTrigger, TransitionBuilder<TState, TTrigger>>? transitions)
     {
@@ -27,6 +65,14 @@ public abstract class StateBuilder<TState, TTrigger> : IStateBuilder<TState, TTr
         _transitions = transitions ?? new Dictionary<TTrigger, TransitionBuilder<TState, TTrigger>>();
     }
 
+    /// <summary>
+    /// Adds a new transition from <c>State</c> to another.
+    /// </summary>
+    /// <param name="trigger">The trigger on which the transition should be initiated.</param>
+    /// <param name="destination">The target state of the transition.</param>
+    /// <param name="gaurd">An optional garud condition. Activates transition only if the garud evaluates to true.</param>
+    /// <returns>The state builder with the new transition.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when either trigger or destination is Null.</exception>
     public IStateBuilder<TState, TTrigger> AddTransition(TTrigger trigger, TState destination, Func<TState, TTrigger, bool>? gaurd)
     {
         if (trigger == null) throw new ArgumentNullException(nameof(trigger));
@@ -54,15 +100,27 @@ public abstract class StateBuilder<TState, TTrigger> : IStateBuilder<TState, TTr
         return this;
     }
 
+    /// <summary>
+    /// Adds a new transition from <c>State</c> to another.
+    /// </summary>
+    /// <param name="trigger">The trigger on which the transition should be initiated.</param>
+    /// <param name="destination">The target state of the transition.</param>
+    /// <returns>The state builder with the new transition.</returns>
     public IStateBuilder<TState, TTrigger> AddTransition(TTrigger trigger, TState destination)
     {
         return AddTransition(trigger, destination, null);
     }
 
+    /// <summary>
+    /// Ignores transitions on the specified trigger.
+    /// </summary>
+    /// <param name="trigger">The trigger to ignore.</param>
+    /// <returns>The state builder.</returns>
     public IStateBuilder<TState, TTrigger> IgnoreTrigger(TTrigger trigger)
     {
         return AddTransition(trigger, State, null);
     }
+
 
     public IStateBuilder<TState, TTrigger> IgnoreTrigger(TTrigger trigger, Func<TState, TTrigger, bool>? gaurd)
     {
--- DotState/Builder/StateMachineBuilder.cs
+++ DotState/Builder/StateMachineBuilder.cs
@@ -3,15 +3,27 @@ using DotState.Exceptions;
 
 namespace DotState.Builder;
 
+/// <summary>
+/// A builder class for the state machine.
+/// </summary>
+/// <typeparam name="TState">The state type</typeparam>
+/// <typeparam name="TTrigger">The trigger type</typeparam>
 public class StateMachineBuilder<TState, TTrigger> : IStateMachineBuilder<TState, TTrigger>
 {
     private readonly IDictionary<TState, StateBuilder<TState, TTrigger>> _stateBuilders;
 
+    /// <summary>
+    /// Initializes a new instance of Class <c>StateMachineBuilder</c>.
+    /// </summary>
     public StateMachineBuilder()
     {
         _stateBuilders = new Dictionary<TState, StateBuilder<TState, TTrigger>>();
     }
 
+    /// <summary>
+    /// Method <c>Build</c> creates an instance of the state machine with the registered state.
+    /// </summary>
+    /// <returns>An instance of state machine.</returns>
     public IStateMachine<TState, TTrigger> Build()
     {
         var stateMachine = new StateMachine<TState, TTrigger>();
@@ -32,6 +44,17 @@ public class StateMachineBuilder<TState, TTrigger> : IStateMachineBuilder<TState
         return stateMachine;
     }
 
+    /// <summary>
+    /// Method <c>CompositeState</c> registers the state as composite state in the state machine.
+    /// 
+    /// If not registered, the default state is registered as an element state by default.
+    /// </summary>
+    /// <param name="state">The state to register</param>
+    /// <param name="defaultState">The default child state of the composite state.</param>
+    /// <returns>A builder for the composite state.</returns>
+    /// <exception cref="MultipleDefaultStateException{TState}">Thrown if multiple default state is registered for the state.</exception>
+    /// <exception cref="ArgumentNullException">Thrown when either the state or the default state is null.</exception>
+    /// <exception cref="StateConfigurationException{TState}">Thrown if the state is already registered.</exception>
     public IStateBuilder<TState, TTrigger> CompositeState(TState state, TState defaultState)
     {
         var stateBuilder = GetStateBuilder(state);
@@ -50,17 +73,39 @@ public class StateMachineBuilder<TState, TTrigger> : IStateMachineBuilder<TState
         return stateBuilder ?? RegisterCompositeState(state, defaultState);
     }
 
+    /// <summary>
+    /// Method <c>ElementState</c> registers the state as element state in the state machine.
+    /// </summary>
+    /// <param name="state">The state to register.</param>
+    /// <returns>A builder for the element state.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if the state is null.</exception>
+    /// <exception cref="StateConfigurationException{TState}">Thrown if state is already registered.</exception>
     public IStateBuilder<TState, TTrigger> ElementState(TState state)
     {
         return GetStateBuilder(state) ?? RegisterElementState(state);
     }
 
+    /// <summary>
+    /// Method <c>GetStateBuiilder</c> gets the registered state builder for the given state.
+    /// </summary>
+    /// <param name="state">The registered state</param>
+    /// <returns>State Builder for the state if it is registered; otherwise, Null.</returns>
     public IStateBuilder<TState, TTrigger>? GetStateBuilder(TState state)
     {
         _stateBuilders.TryGetValue(state, out var builder);
         return builder;
     }
 
+    /// <summary>
+    /// Method <c>RegisterCompositeState</c> register the state as composite state in the state machine.
+    /// 
+    /// If not registered, the default state is registered as an element state by default.
+    /// </summary>
+    /// <param name="state">The state to register</param>
+    /// <param name="defaultState">The default child state of the composite state.</param>
+    /// <returns>A composite state builder for the state.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when either the state or the default state is null.</exception>
+    /// <exception cref="StateConfigurationException{TState}">Thrown if the state is already registered.</exception>
     internal CompositeStateBuilder<TState, TTrigger> RegisterCompositeState(TState state, TState defaultState)
     {
         if (state == null) throw new ArgumentNullException(nameof(state));
@@ -79,6 +124,16 @@ public class StateMachineBuilder<TState, TTrigger> : IStateMachineBuilder<TState
         return compositeStateBuilder;
     }
 
+    /// <summary>
+    /// Method <c>RegisterCompositeState</c> registers the state builder as composite state builder in the state machine. Can be used to re-register 
+    /// an element state, as a composite state.
+    /// 
+    /// If not registered, the default state is registered as an element state by default.
+    /// </summary>
+    /// <param name="stateBuilder">The state builder to register as composite.</param>
+    /// <param name="defaultState">The default child state of the composite state.</param>
+    /// <returns>A state builder for the composite state.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when either the state or the default state is null.</exception>
     private CompositeStateBuilder<TState, TTrigger> RegisterCompositeState(IStateBuilder<TState, TTrigger> stateBuilder, TState defaultState)
     {
         if (stateBuilder == null) throw new ArgumentNullException(nameof(stateBuilder));
@@ -95,6 +150,13 @@ public class StateMachineBuilder<TState, TTrigger> : IStateMachineBuilder<TState
         return compositeStateBuilder;
     }
 
+    /// <summary>
+    /// Method <c>RegisterElementState</c> register the state as element state in the state machine.
+    /// </summary>
+    /// <param name="state">The state to register</param>
+    /// <returns>A state builder for the element state.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if the state is null.</exception>
+    /// <exception cref="StateConfigurationException{TState}">Thrown if state is already registered.</exception>
     internal StateBuilder<TState, TTrigger> RegisterElementState(TState state) 
     {
         if (state == null) throw new ArgumentNullException(nameof(state));
